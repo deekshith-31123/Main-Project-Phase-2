c38d92ff8a58884989f1dd93c0f460d06a6419d0
deekshith-31123 <patnanasaideekshith@gmail.com>
2024-12-19 21:03:57 +0530

    Project Creation,Frontend code and Model code added

---
+++ FinalProject/src/main/java/com/example/demo/model/Todo.java
@@ -0,0 +1,45 @@
+package com.example.demo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "todos")
+public class Todo {
+
+    @Id
+    private String id;
+    private String task;
+    private boolean completed;
+
+    public Todo() {}
+
+    public Todo(String task) {
+        this.task = task;
+        this.completed = false;
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
